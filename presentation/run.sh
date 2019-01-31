3d8f40ea963d62bcc85e7bbbcd6afeff7ba91ed8
Maximilian Ammann <max@maxammann.org>
2019-01-31 15:14:49 +0100

    Romove tracking

--- presentation/run.sh
+++ presentation/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python2 Impressive-0.12.0/impressive.py -t Crossfade --transtime 100 --time-display build/cps-presentation.pdf
+python2 Impressive-0.12.0/impressive.py -t Crossfade --transtime 100 build/cps-presentation.pdf
